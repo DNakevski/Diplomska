65a6ad49a3f7216cbe094d698e7d4f7bdff58dec
andrijanaandi <andrijana.andi@gmail.com>
2015-08-31 22:28:11 +0200

    Made some changes

--- MojKatalog/Areas/Portfolio/Controllers/PortfolioController.cs
+++ MojKatalog/Areas/Portfolio/Controllers/PortfolioController.cs
@@ -18,39 +18,37 @@ namespace MojKatalog.Areas.Portfolio.Controllers
         }
         public ActionResult DodadiPortfolio(int id) 
         {
-            WebSiteSettings portfolioSettings = new WebSiteSettings();
-            portfolioSettings.IdKatalozi = id;
-            portfolioSettings.FontFamily = "OpenSans-Regular";
-            portfolioSettings.FontColor1 = "darkBlueDarker";
-            portfolioSettings.FontColor2 = "greenLighter";
-            portfolioSettings.BGPocetna = "/Areas/Portfolio/Images/HomeBackgroundImage.jpg";
-            portfolioSettings.BGZaNas = "#FFFFFF";
-            portfolioSettings.BGFZaNas = "/Areas/Portfolio/Images/AboutFooter.jpg";
-            portfolioSettings.BGPortfolio = "#FFFFFF";
-            portfolioSettings.BGFPortfolio = "/Areas/Portfolio/Images/AboutFooter.jpg";
-            portfolioSettings.BGContact = "#ECF0F1";
-            portfolioSettings.BGMenu = "#f7f9f7";
-            portfolioSettings.BGFooter = "#ECF0F1";
-            portfolioSettings.Naziv = "Назив на Веб Сајтот";
-            /*WebSiteSettings{IdKatalozi= id; FontFamily= "OpenSans-Regular"; 
-                                                     FontColor1= "darkBlueDarker"; 
-                                                     FontColor2= "greenLighter";
-                                                     BGPocetna= "~/Areas/Portfolio/Images/HomeBackgroundImage.jpg";
-                                                     BGZaNas= "#FFFFFF";
-                                                     BGFZaNas= "~/Areas/Portfolio/Images/AboutFooter.jpg";
-                                                     BGPortfolio= "#FFFFFF";
-                                                     BGFPortfolio= "~/Areas/Portfolio/Images/AboutFooter.jpg";
-                                                     BGContact= "#ECF0F1";
-                                                     BGMenu= "#f7f9f7";
-                                                     BGFooter= "#ECF0F1"};*/
+            WebSiteSettings portfolioSettings = new WebSiteSettings
+            {
+                IdKatalozi = id,
+                FontFamily = "OpenSans-Regular",
+                FontColor1 = "darkBlueDarker",
+                FontColor2 = "greenLighter",
+                BGPocetna = "~/Areas/Portfolio/Images/HomeBackgroundImage.jpg",
+                BGZaNas = "white",
+                BGFZaNas = "~/Areas/Portfolio/Images/AboutFooter.jpg",
+                BGPortfolio = "white",
+                BGFPortfolio = "~/Areas/Portfolio/Images/AboutFooter.jpg",
+                BGContact = "lightGreyLighter",
+                BGMenu = "lightGreyLighter",
+                BGFooter = "lightGreyLighter"
+            };
             //Insert vo tabela WebSiteSettings so default vrednosti
             model.DodadiPortfolio(portfolioSettings);
             return View();
         }
+        [HttpGet]
         public ActionResult IzmeniPortfolio(int id) 
         {
             WebSiteSettings portfolioSettings = model.IzmeniPortfolioGet(id);
             //Editiranje na vrednosti vo tabela WebSiteSettings
+            return View(portfolioSettings);
+        }
+        [HttpPost]
+        public ActionResult IzmeniPortfolio(WebSiteSettings wsettings)
+        {
+            model.IzmeniPortfolioPost(wsettings);
+            WebSiteSettings portfolioSettings = model.IzmeniPortfolioGet(wsettings.IdKatalozi);
             return View(portfolioSettings);
         }
 
--- MojKatalog/Areas/Portfolio/Queries/QPortfolio.cs
+++ MojKatalog/Areas/Portfolio/Queries/QPortfolio.cs
@@ -20,5 +20,23 @@ namespace MojKatalog.Areas.Portfolio.Queries
         {
             return _db.WebSiteSettings.Find(idKatalog);
         }
+        public void IzmeniPortfolioPost(WebSiteSettings newWSettings)
+        {
+            WebSiteSettings wsettings = _db.WebSiteSettings.Find(newWSettings.IdKatalozi);
+            wsettings.Naziv = newWSettings.Naziv;
+            wsettings.FontFamily = newWSettings.FontFamily;
+            wsettings.FontColor1 = newWSettings.FontColor1;
+            wsettings.FontColor2 = newWSettings.FontColor2;
+            wsettings.BGPocetna = newWSettings.BGPocetna;
+            wsettings.BGZaNas = newWSettings.BGZaNas;
+            wsettings.BGFZaNas = newWSettings.BGFZaNas;
+            wsettings.BGPortfolio = newWSettings.BGPortfolio;
+            wsettings.BGFPortfolio = newWSettings.BGFPortfolio;
+            wsettings.BGContact = newWSettings.BGContact;
+            wsettings.BGMenu = newWSettings.BGMenu;
+            wsettings.BGFooter = newWSettings.BGFooter;
+            _db.SaveChanges();
+
+        }
     }
 }
