82a6707dd6275194369917ea3af8f45b1e9a4ad7
andrijanaandi <andrijana.andi@gmail.com>
2015-09-16 21:16:00 +0200

    ImagesAndColorSave

---
+++ MojKatalog/Helpers/ColorHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MojKatalog.Helpers
+{
+    public class ColorHelper
+    {
+        public static bool IsColor(string input)
+        {
+            List<string> colors = new List<string>() { 
+                "darkBlueLighter",
+                "darkBlueDarker",
+                "lightBlueLighter",
+                "lightBlueDarker",
+                "greenLighter",
+                "greenDarker",
+                "orangeLighter",
+                "orangeDarker",
+                "redLighter",
+                "redDarker",
+                "darkGreyLighter",
+                "darkGreyDarker",
+                "lightGreyLighter",
+                "lightGreyDarker",
+                "white",
+                "black"
+            };
+            //dodadi gi svite boe...
+
+            return colors.Contains(input);
+        }
+    }
+}
